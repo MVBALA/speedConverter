ab9ea4b1b06cb1e3a2da8417582a5aeb92c863f0
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-17 20:04:38 +0530

    This is my first solution of Udemy course.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+     double miles = speedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = "+ miles);
+        speedConverter.printConversion(10.5);
+    }
+}
